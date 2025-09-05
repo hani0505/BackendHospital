170a5fed86a9fe4fba0a9782bd1c467b7945919c
Hani0505 <raniellysouza001@gmail.com>
2025-09-05 16:01:45 -0300

    projeto base

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+//rota de autenticação 
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+app.use(cors)
+
+app.use("/auth")
+app.use('/paciente')
+app.use('/med')
+app.use('/atendimento')
+app.use('/relatorio')
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em localhost:${PORT}`)
+})
